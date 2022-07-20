2e1436e68890092e41fedb38ea8a0ae670a2d898
naimulislamshakil <ms0413354@gmail.com>
2022-07-20 07:55:39 +0600

    user login

--- src/Component/pages/Home/Login.js
+++ src/Component/pages/Home/Login.js
@@ -1,7 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+
+  const userLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const user = { email, password };
+
+    fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.message === "User valid") {
+          navigate(from, { replace: true });
+        } else {
+          toast.error(data.message);
+        }
+      });
+  };
   return (
     <div>
       <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
@@ -9,7 +35,7 @@ const Login = () => {
           <h1 className="text-3xl font-semibold text-center text-purple-700 underline">
             Login
           </h1>
-          <form className="mt-6">
+          <form onSubmit={userLogin} className="mt-6">
             <div className="mb-2">
               <label
                 for="email"
@@ -19,7 +45,7 @@ const Login = () => {
               </label>
               <input
                 type="email"
-                name="e"
+                name="email"
                 className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
               />
             </div>
@@ -32,6 +58,7 @@ const Login = () => {
               </label>
               <input
                 type="password"
+                name="password"
                 className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
               />
             </div>
@@ -39,9 +66,11 @@ const Login = () => {
               Forget Password?
             </Link>
             <div className="mt-6">
-              <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600">
-                Login
-              </button>
+              <input
+                value="Login"
+                type="submit"
+                className="w-full text-center px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
+              />
             </div>
           </form>
 
